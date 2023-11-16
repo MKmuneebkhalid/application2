9c65d6317fb41acd138faf51fa9e89fe0aaca950
Muneebkhalid006 <muneebkhalid006@gmail.com>
2023-11-17 00:33:07 +0500

    Procfile update10

--- PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2.py
@@ -1,4 +1,4 @@
-import os
+﻿import os
 import numpy as np
 from flask import Flask, render_template, request
 
@@ -54,7 +54,3 @@ def fit_polynomial(numbers, degree):
     polynomial_terms = [f"{coeff:.4f}x^{degree-i}" for i, coeff in enumerate(coeffs)]
     polynomial_str = " + ".join(polynomial_terms).replace('x^0', '').replace(' + -', ' - ')
     return polynomial_str
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
-
